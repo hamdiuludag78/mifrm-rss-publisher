2ac115972a32d665740137d58cf3bd6c55a5a98c
Hamdi Uludağ <hamdiuludag@gmail.com>
2026-07-12 01:15:56 +0300

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = Object.freeze({
   SUPABASE_URL: "https://qonnbmfsydvoygxrqyvm.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_KkOf8QVNNqrS6unW3btgDQ_ntuE8Ube",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvbm5ibWZzeWR2b3lneHJxeXZtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4Mzc5MjI0OCwiZXhwIjoyMDk5MzY4MjQ4fQ.4QT2JqXsXVOCrpdDGT-ojVnEyZtqqnmTuSsDpKw_ee8",
   ADMIN_EMAIL: "xh.a.rx@hotmail.com"
 });

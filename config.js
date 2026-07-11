19b3acdf86e555af25e1f222bcc4c2bec1bc7f62
Hamdi Uludağ <hamdiuludag@gmail.com>
2026-07-11 23:03:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = Object.freeze({
-  SUPABASE_URL: "https://PROJE-ID.supabase.co",
-  SUPABASE_ANON_KEY: "BURAYA_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://qonnbmfsydvoygxrqyvm.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_KkOf8QVNNqrS6unW3btgDQ_ntuE8Ube",
   ADMIN_EMAIL: "xh.a.rx@hotmail.com"
 });
